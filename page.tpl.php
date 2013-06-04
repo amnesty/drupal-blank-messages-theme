0a5092695c5e104d1026f5be177ce6354eaae5b1
Capo <mail@carloscapote.com>
2013-06-04 10:53:11 +0200

    Remove Unnecesary BODY and HEAD tags

--- page.tpl.php
+++ page.tpl.php
@@ -1,23 +1,21 @@
-<head>
-    <?php print $styles; ?>
-    <?php print $scripts; ?>
-</head>
-<body>
-    <div id="skip-link">
-        <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
-    </div>
+<?php print $styles; ?>
+<?php print $scripts; ?>
 
-    <div id="page">
-        <div id="content" class="clearfix">
-            <?php if ($messages): ?>
-                <div id="messages">
-                    <div id="console" class="section clearfix">
-                        <?php print $messages; ?>
-                    </div>
+<div id="skip-link">
+    <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
+</div>
+
+<div id="page">
+    <div id="content" class="clearfix">
+        <?php if ($messages): ?>
+            <div id="messages">
+                <div id="console" class="section clearfix">
+                    <?php print $messages; ?>
                 </div>
-            <?php endif; ?>
+            </div>
+        <?php endif; ?>
 
-            <div id="content-area"><?php print render($page['content']); ?></div>
-        </div>
+        <div id="content-area"><?php print render($page['content']); ?></div>
     </div>
-</body>
+</div>
+
